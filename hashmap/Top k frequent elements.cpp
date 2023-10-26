6e0407b89020868ccaefc1f7dd9028a2dce7f581
Shreya Satyakam <89099439+Shreyasatyakam@users.noreply.github.com>
2023-10-26 18:43:00 +0530

    Create Top k frequent elements.cpp

---
+++ hashmap/Top k frequent elements.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+       priority_queue<pair<int,int>>pq;//minheap
+       unordered_map<int,int>mp;
+       for(auto x:nums){
+           mp[x]++;
+       } 
+       for(auto x:mp){
+           pq.push({x.second,x.first});
+       }
+       vector<int> v;
+       while(k--){
+           v.push_back(pq.top().second);
+           pq.pop();
+       }
+       return v;
+    }
+};
